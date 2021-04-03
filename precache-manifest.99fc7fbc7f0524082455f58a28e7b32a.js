e3e16ad7c9fbabd58e596e77815bd99d108a8e3b
Nicu Tofan <nicu.tofan@gmail.com>
2021-04-03 09:04:34 +0300

    Updates

---
+++ precache-manifest.99fc7fbc7f0524082455f58a28e7b32a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "158745f7300fceb615b3b727957b8c5f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e6f190c7de83910d3626",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "d57f99c21f8618920f83",
+    "url": "./static/js/2.d94ce316.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d94ce316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6f190c7de83910d3626",
+    "url": "./static/js/main.a84cd539.chunk.js"
+  },
+  {
+    "revision": "bb55aeeb79112bf13528",
+    "url": "./static/js/runtime-main.1b2e20b6.js"
+  }
+]);
